78e4892e6487c7219eed2d0e57524bbc3c76fa90
Ali Kelkawi <81743070+kelkawi-a@users.noreply.github.com>
2026-09-10 16:09:08 +0300

    use force flag when deploying postgresql on juju 4 (#45)

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -231,9 +231,14 @@ def deploy_lxd_dependencies(lxd_juju: jubilant.Juju) -> None:
     Args:
         lxd_juju: Jubilant object for the LXD model.
     """
+    juju_version = lxd_juju.version()
+    force_postgresql = juju_version.major == 4
+    if force_postgresql:
+        logger.warning("Deploying PostgreSQL with --force for Juju %s", juju_version)
+
     lxd_juju.deploy(KAFKA_NAME, channel=KAFKA_CHANNEL)
     lxd_juju.deploy(OPENSEARCH_NAME, channel=OPENSEARCH_CHANNEL, num_units=2)
-    lxd_juju.deploy(POSTGRES_NAME, channel=POSTGRES_CHANNEL)
+    lxd_juju.deploy(POSTGRES_NAME, channel=POSTGRES_CHANNEL, force=force_postgresql)
     lxd_juju.deploy(CERTIFICATES_NAME, channel=CERTIFICATES_CHANNEL)
     lxd_juju.deploy(ZOOKEPER_NAME, channel=ZOOKEEPER_CHANNEL)
 
